f5de0968c0667c42f79da33b2a48a3102e5132bb
Amit <amitkumarashutos@gmail.com>
2024-09-19 10:32:51 +0530

    Home page and header, footer component added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Layout>home page</Layout>} />
+        <Route path='/' element={<Layout><Home /></Layout>} />
         <Route path='/login' element={<p>login</p>} />
       </Routes>
     </Router>
