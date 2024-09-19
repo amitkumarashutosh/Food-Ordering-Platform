8a91ab6bc868fc6f69898a8689098475c82e430b
Amit <amitkumarashutos@gmail.com>
2024-09-19 13:08:42 +0530

    SignIn, signUp and  logout feature added

---
+++ client/src/components/MobileNavLinks.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const MobileNavLinks = () => {
+  const { logout } = useAuth0();
+  return (
+    <div>
+      <Link
+        to="/user-profile"
+        className="flex bg-white items-center font-bold hover:text-orange-500 mb-2 text-base"
+      >
+        User Profile
+      </Link>
+      <Button
+        className="w-full items-center px-3 font-bold hover:bg-gray-500"
+        onClick={() => logout()}
+      >
+        Log Out
+      </Button>
+    </div>
+  );
+};
+
+export default MobileNavLinks;
